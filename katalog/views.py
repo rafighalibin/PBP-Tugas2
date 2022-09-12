2f482720ceb200c7ef967cc1a568f6b118014287
Rafi Ghalibin Abrar <rafighalibin@gmail.com>
2022-09-12 22:33:47 +0700

    routing views.py

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+data_barang_katalog = CatalogItem.objects.all()
+def show_katalog(request):
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Rafi Ghalibin Abrar',
+        'id': '2106751354'
+    }
+    return render(request, "katalog.html", context)

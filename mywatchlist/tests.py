81466ce9ac816151b909e311e516280583ca529e
Rafi Ghalibin Abrar <rafighalibin@gmail.com>
2022-09-21 14:49:23 +0700

    initial test

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from urllib import response
+from django.test import TestCase, Client
+from django.urls import resolve
+#from .views import show_watchlist
 
-# Create your tests here.
+class MyWatchlistTest(TestCase):
+    def test_mywatchlist_html_url_exist(self):
+        response = Client().get('/mywatchlist/html/')
+        self.assertEqual(response.status_code,200)
+    
+    def test_mywatchlist_xml_url_exist(self):
+        response = Client().get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)
+    
+    def test_mywatchlist_json_url_exist(self):
+        response = Client().get('/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
+
+    # def test_mywatchlist_using_to_do_list_template(self):
+    #     response = Client().get('/mywatchlist/')
+    #     self.assertTemplateUsed(response, 'mywatchlist.html')
+
+
